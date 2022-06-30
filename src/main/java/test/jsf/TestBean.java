fcdaa1a532d0e3b87b02c02a8f73540a1440a410
James McLean <empyreanx@zoho.com>
2022-06-30 12:12:56 -0400

    AJAX

--- src/main/java/test/jsf/TestBean.java
+++ src/main/java/test/jsf/TestBean.java
@@ -3,38 +3,37 @@ package test.jsf;
 import java.io.Serializable;
 
 import javax.ejb.EJB;
-import javax.enterprise.context.SessionScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import test.ejb.TestEJB;
 import test.jpa.TestEntity;
 
 @Named
-@SessionScoped
-//@RequestScoped
+@ViewScoped
 public class TestBean implements Serializable {
     private static final long serialVersionUID = 4874297007974595139L;
     
     @EJB
     private TestEJB testEJB;
-    private String message;
+    private String text;
 
     public TestBean() {
         
     }
     
-    public String getMessage() {
-        return message;
+    public String getText() {
+        return text;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public void setText(String text) {
+        this.text = text;
     }
     
     public String write() {
-        TestEntity test = new TestEntity(message);
+        TestEntity test = new TestEntity(this.text);
         testEJB.save(test);
-        message = null;
+        this.text = null;
         return "/";
     }
     
@@ -42,7 +41,7 @@ public class TestBean implements Serializable {
         TestEntity test = testEJB.read();
         
         if (test != null)
-            message = test.getMessage();
+            this.text = test.getMessage();
         //else
         //    message = null;
         
